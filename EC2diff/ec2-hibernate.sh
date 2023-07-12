159112d24edd07179e5f161e0d5fae2ed172687d
Irina Zakharova <13151882+yrenamm@users.noreply.github.com>
2023-07-12 07:53:02 -0400

    Update ec2-hibernate.sh

--- EC2diff/ec2-hibernate.sh
+++ EC2diff/ec2-hibernate.sh
@@ -1,8 +1,11 @@
 # Create inst with hibernate option
-aws ec2 run-instances --image-id ami-0272362388eae3591 --instance-type m5.large --block-device-mappings file://mapping.json --hibernation-options Configured=true --count 1 --key-name keypairsavedca
+aws ec2 run-instances --image-id ami-0272362388eae**** --instance-type m5.large --block-device-mappings file://mapping.json --hibernation-options Configured=true --count 1 --key-name keypairsavedca
+
+# Start instance
+aws ec2 start-instances instance-id i-0e3db58cafde0****
 
 # Terminate your EC2 instances
-aws ec2 terminate-instances --instance-ids i-049fca92aca8a78fd
+aws ec2 terminate-instances --instance-ids i-0e3db58cafde0****
 
 # Delete volume
-aws ec2 delete-volume --volume-id vol-04aadba4c020e335c
+aws ec2 delete-volume --volume-id vol-04aadba4c020e****
